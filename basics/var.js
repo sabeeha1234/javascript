7ff45462469212937134f70cec1921b4aa4a734b
sabeeha1234 <142984283+sabeeha1234@users.noreply.github.com>
2024-02-08 15:14:40 +0000

    var

---
+++ basics/var.js
@@ -0,0 +1,14 @@
+const accountId = 12278
+let accountEmail = "shaheed@google.com"
+var accountPassword ="123455"
+accountCity="vijayawada"
+let accountState;
+
+//console.log(accountId)
+
+
+accountEmail = "sh.com"
+var accountPassword ="12345"
+accountCity="vijayawaa"
+
+console.table({accountEmail,accountId,accountCity})
