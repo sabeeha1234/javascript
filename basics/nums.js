9203402014a936f7aec8fe8d2d3b2ec0ae5790bd
sabeeha1234 <142984283+sabeeha1234@users.noreply.github.com>
2024-02-10 15:04:01 +0000

    main

---
+++ basics/nums.js
@@ -0,0 +1,28 @@
+// const score=400
+// console.log(score);
+// const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// const abb=123.455555
+// console.log(abb.toPrecision(4));
+// const hundr=1000000
+// console.log(hundr.toLocaleString('en-IN'));
+
+
+
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.ceil(Math.random()*10)+1);
+
+
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
