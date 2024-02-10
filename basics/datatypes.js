35df4270eb73fa03f3d10a0826f21fc64e281201
sabeeha1234 <142984283+sabeeha1234@users.noreply.github.com>
2024-02-10 18:35:53 +0000

    java.obj

---
+++ basics/datatypes.js
@@ -0,0 +1,8 @@
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+constmyArr=[0,1,2,3,4,5]
+ 
+const myHeors = ["shaheed", "shaheed"]
+const myArr2 = new Array(1, 2, 3, 4) 
+console.log(myArr[1]);
---
+++ basics/objects.js
@@ -0,0 +1,25 @@
+// const fruits=["banana","apple","guava"]
+// const vegies=["carrot","beetroot","cucumber"]
+// fruits.push(vegies)
+// console.log(fruits);
+// console.log(fruits[2][2]);
+// const allFruits=fruits.concat(vegies)
+// console.log(allFruits);
+
+
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// const real_another_array = another_array.flat(Infinity) 
+// console.log(real_another_array);
+
+// console.log(Array.isArray("shaheed")) 
+// console.log(Array.from("shaheed")) 
+// console.log(Array.from({name: "shaheed"})) 
+
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3));
+ 
