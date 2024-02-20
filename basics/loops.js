fea6427c27f88fc7e3ad8dd2e5be0432cdb3878f
sabeeha1234 <142984283+sabeeha1234@users.noreply.github.com>
2024-02-20 10:38:23 +0000

    main

---
+++ basics/loops.js
@@ -0,0 +1,40 @@
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(index);
+    
+// }
+
+// for (let i = 0; i< 10; i++) {
+//     console.log(`outerloop value ${i}`);
+//     // for (let j= 0;  j< 20; j++) {
+//        // console.log(`innerloop ${j} and outerloop value is ${i}`);
+//        console.log(i+`*`+j+`=`+i*j);
+//     }
+    
+
+//}
+
+
+
+
+
+const array=["sabbu","shah","nandu"]
+let arr=0
+while (arr<array.length) {
+   
+    console.log(`value is ${array[arr]}`);
+    arr=arr+1
+}
+
+// for (let index = 1; index <=20; index++) {
+//      if (index==5) {
+//         console.log(`detected 5`);
+//         break
+        
+//      }
+//      console.log(`value of index is ${index}`);
+//     }
+    
+        
+    
+   
---
+++ basics/while.js
@@ -0,0 +1,67 @@
+// let user=12
+// do{
+//     console.log(`user is ${user}`);
+//     user++
+// }while(user<=10);
+// const arr=[1,2,3,4,5]
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+// const greetings="hello js"
+// for (const greet of greetings) {
+
+//    console.log(`each char is ${greet}`);
+    
+// }
+
+
+
+// const map=new Map()
+// map.set('in',"india")
+// map.set('usa',"united states of india")
+// map.set('fr',"france")
+// map.set('in',"india")
+// //console.log(map);
+
+
+
+// for(const[key,value]of map){
+//     console.log(key,':-',value);
+// }
+
+
+
+
+// const myObject={
+//     'game1':'gta'
+//     'game2':'vicecity'
+// }
+
+
+
+
+// const myObject={
+//     js:'javascript',
+//     cpp:'c++',
+//     swift:'swift by apple',
+//     rb:'ruby'
+// }
+// for (const key in myObject) {
+//    //console.log(key);
+//  console.log(`${key}shortcut is for ${myObject[key]}`);   
+// }
+// const programming=["js","rb","py","java","cpp"]
+// for (const key in programming) {
+//     console.log(programming[key]);
+    
+// }
+
+
+// const coding=["js","rb","py","java","cpp"]
+// coding.forEach(function(val){
+//     console.log(val);
+// })
+
+coding.array.forEach(item)=>
+});
